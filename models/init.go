db40d9b39d075ccf1c18e46aa4d0d44045d065c7
lansen <lansen_i@didiglobal.com>
2023-12-12 14:36:31 +0800

    project init

---
+++ models/init.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"getcharzp.cn/define"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB = Init()
+
+var RDB = InitRedisDB()
+
+func Init() *gorm.DB {
+	dsn := define.MysqlDNS + "/gin_gorm_oj?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("gorm Init Error : ", err)
+	}
+	return db
+}
+
+func InitRedisDB() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
